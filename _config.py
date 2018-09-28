9f4c6b2d94de317fba7e9dbe6e8cf0968b6c4c66
Mike Bayer <mike_mp@zzzcomputing.com>
2018-09-28 15:16:47 -0400

    - remove unused controllers for zeekofile build

--- _config.py
+++ _config.py
@@ -3,16 +3,6 @@
 # for config options
 # or run 'blogofile help init' to see more complete templates
 
-docs = controllers.docs
-
-docs.enabled = True
-
-dev_root = '/Users/classic/dev'
-
-docs.sphinx_docs = [
-    (dev_root + '/mako/doc/build/output/site', ''),
-]
-
 site.url = "http://www.makotemplates.org"
 
 filters.syntax_highlight.style= "manni"
@@ -20,19 +10,5 @@ filters.syntax_highlight.enabled = True
 
 # blog
 blog = controllers.blog
-blog.enabled = True
+blog.enabled = False
 blog.path = "/blog"
-
-blog.name = "Your Blog's Name"
-
-blog.description = "Your Blog's short description"
-
-blog.timezone = "US/Eastern"
-
-blog.post_default_filters = {
-    "markdown": "markdown",
-    "textile": "syntax_highlight, textile",
-    "org": "syntax_highlight, org",
-    "rst": "rst, syntax_highlight"
-}
-
